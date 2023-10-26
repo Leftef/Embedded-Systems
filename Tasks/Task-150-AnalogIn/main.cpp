5bde295966c6513271050b3a33c6b62567e07c1c
Leftef <alexlove331@gmail.com>
2023-10-26 15:01:20 +0100

    Update main.cpp

--- Tasks/Task-150-AnalogIn/main.cpp
+++ Tasks/Task-150-AnalogIn/main.cpp
@@ -9,6 +9,10 @@ LCD_16X2_DISPLAY disp;
 Buzzer buzz;
 LatchedLED ledDisp(LatchedLED::STRIP);
 
+DigitalOut green_led(PC_6);
+DigitalOut yellow_led(PC_3);
+DigitalOut red_led(PC_2);
+
 //Analogue Inputs
 AnalogIn pot(AN_POT_PIN);
 AnalogIn ldr(AN_LDR_PIN);
@@ -20,26 +24,6 @@ int main()
     //Test LED Bar Display
     ledDisp.enable(true);
 
-    ledDisp.setGroup(LatchedLED::LEDGROUP::RED);
-    for (unsigned int n=0; n<8; n++) {
-        ledDisp = 1 << n;
-        wait_us(250000);
-    }
-    ledDisp = 0;
-
-    ledDisp.setGroup(LatchedLED::LEDGROUP::GREEN);
-    for (unsigned int n=0; n<8; n++) {
-        ledDisp = 1 << n;
-        wait_us(250000);
-    }
-    ledDisp = 0;
-    
-    ledDisp.setGroup(LatchedLED::LEDGROUP::BLUE);
-    for (unsigned int n=0; n<8; n++) {
-        ledDisp = 1 << n;
-        wait_us(250000);
-    }     
-    ledDisp = 0;
 
 
     while (true) {
@@ -56,7 +40,34 @@ int main()
 
         //Wait 0.25 seconds
         wait_us(500000);
+        if(lightVal<4096){
+            ledDisp.setGroup(LatchedLED::LEDGROUP::GREEN);
+            for (unsigned int n=0; n<8; n++) {
+                ledDisp = 1 << n;
+                wait_us(250000);
+            }
+            ledDisp = 0;
+        }
+
+        if(potVal>32768){
+         ledDisp.setGroup(LatchedLED::LEDGROUP::RED);
+            for (unsigned int n=0; n<8; n++) {
+                ledDisp = 1 << n;
+                wait_us(250000);
+            }
+            ledDisp = 0;
+        }
+
+        if(micVal>4095){
+            ledDisp.setGroup(LatchedLED::LEDGROUP::BLUE);
+            for (unsigned int n=0; n<8; n++) {
+                ledDisp = 1 << n;
+                wait_us(250000);
+            }     
+            ledDisp = 0;
+        }
 
     }
+
 }
 
